d1a9db6dbce61e0c393ccee581c261bf7f9c3555
Davide Becker <hi@davide.me>
2018-07-26 10:37:15 +0200

    Add debug canvas

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,22 @@
+var canvas = document.getElementById('debugCanvas');
+var context = canvas.getContext('2d');
+
 const RADIAN = Math.PI / 2
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
+    0.1, 100);
 const renderer = new THREE.WebGLRenderer;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(50,5,25);
-const material = new THREE.MeshBasicMaterial({ color: 0x00a3b4, wireframe: false, vertexColors: THREE.FaceColors });
+const geometry = new THREE.BoxGeometry(50, 5, 25);
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00a3b4,
+    wireframe: false,
+    vertexColors: THREE.FaceColors
+});
 const cube = new THREE.Mesh(geometry, material);
 geometry.faces[4].color.setHex(0x000099)
 geometry.faces[5].color.setHex(0x000099)
@@ -23,8 +31,15 @@ const render = () => {
 
 socket = io();
 
-window.onload = function () {
-    var tracker = new tracking.ColorTracker(['yellow']);
+window.onload = function() {
+    tracking.ColorTracker.registerColor('green', function(r, g, b) {
+        if (r < 40 && g > 120 /*&& b < 100*/) {
+            return true;
+        }
+        return false;
+    });
+
+    var tracker = new tracking.ColorTracker(['magenta']);
     tracker.setMinDimension(5);
     tracker.setMinGroupSize(10);
 
@@ -38,7 +53,6 @@ window.onload = function () {
 };
 
 var quat = new THREE.Quaternion(0, 0, 0, 0)
-
 
 socket.on('sensorData', function(data) {
     // console.log(cube.rotation.x, cube.rotation.y, cube.rotation.z)
@@ -57,11 +71,28 @@ function onColorMove(event) {
     var maxRect;
     var maxRectArea = 0;
 
-    event.data.forEach(function (rect) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    event.data.forEach(function(rect) {
         if (rect.width * rect.height > maxRectArea) {
             maxRectArea = rect.width * rect.height;
             maxRect = rect;
         }
+
+        if (rect.color === 'custom') {
+            rect.color = tracker.customColor;
+        }
+
+        console.log(rect)
+
+        context.strokeStyle = rect.color;
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        context.font = '11px Helvetica';
+        context.fillStyle = "#fff";
+        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5,
+            rect.y + 11);
+        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5,
+            rect.y + 22);
     });
 
     if (maxRectArea > 0) {
